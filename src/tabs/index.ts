89e7ca249cde64caf5105942e68f0dd4cadff800
Hugo Mano <hugo@hugomano.com>
2018-05-17 12:05:10 +0200

    Add documentation

--- src/tabs/index.ts
+++ src/tabs/index.ts
@@ -5,8 +5,29 @@ export namespace tabs {
   export interface TabsConsumer extends Consumer {
     readonly id: string;
 
+    /**
+     * List all tabs
+     * @example
+     * const serviceTabs = sdk.tabs.getTabs();
+     */
     getTabs(): Tab[];
+    /**
+     * Navigate to given tabId
+     * @example
+     * sdk.tabs.navToTab('mysaas-XXXXXXXXX/XXXXXXXXX');
+     */
     navToTab(tabId: string): Promise<void>
+    /**
+     * Execute javascript code in web view for given tabId
+     * @example
+     *  const code = `
+     *    var state = { page: '/messages/${id}' };
+     *    history.pushState(state, '', 'https://mysaas.com/messages/${id}');
+     *    var popStateEvent = new PopStateEvent('popstate', { state: state });
+     *    dispatchEvent(popStateEvent);
+     *  `;
+     * sdk.tabs.executeJavaScript('mysaas-XXXXXXXXX/XXXXXXXXX', code);
+     */
     executeJavaScript(tabId: string, code: string): Promise<void>;
     setProviderInterface(providerInterface: tabs.TabsProviderInterface): void
   }
