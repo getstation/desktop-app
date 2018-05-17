a50ff00ea96a073ce060969f9ad5b46a96150e94
Hugo Mano <hugo@hugomano.com>
2018-05-17 10:51:22 +0200

    Type everything

--- src/tabs/index.ts
+++ src/tabs/index.ts
@@ -5,16 +5,28 @@ export namespace tabs {
   export interface TabsConsumer extends Consumer {
     readonly id: string;
 
-    getTabs(): any;
+    getTabs(): Tab[];
     navToTab(tabId: string): Promise<void>
     executeJavaScript(tabId: string, code: string): Promise<void>;
     setProviderInterface(providerInterface: tabs.TabsProviderInterface): void
   }
 
   export interface TabsProviderInterface {
-    getTabs(id: string): any;
+    getTabs(id: string): Tab[];
     navToTab(tabId: string): Promise<void>
     executeJavaScript(tabId: string, code: string): Promise<void>;
   }
 
+  export type Tab = {
+    applicationId: string,
+    badge: string,
+    canGoBack: boolean,
+    canGoForward: boolean,
+    favicons: string[],
+    isApplicationHome: boolean,
+    isLoading: boolean,
+    tabId: string,
+    title: string,
+    url: string,
+  }
 }
