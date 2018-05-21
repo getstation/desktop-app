9a0ddc0402c8fc258db4cb686ab1c79d992a970d
Joël Charles <joel.charles91@gmail.com>
2018-05-21 16:24:36 +0200

    storage SDK now follows webExtension Storage API. BREAKING CHANGE

--- src/storage/index.ts
+++ src/storage/index.ts
@@ -1,18 +1,20 @@
 import { Consumer } from '../common';
+import { StorageEvent } from './event';
 
 export namespace storage {
 
   export interface StorageConsumer extends Consumer {
     readonly id: string;
+    readonly onChanged: StorageEvent;
 
-    getItem<R = any>(key: string): R;
-    setItem(key: string, value: any): void;
+    getItem<R = any>(key: string): Promise<R | undefined>;
+    setItem(key: string, value: any): Promise<void>;
     setProviderInterface(providerInterface: storage.StorageProviderInterface): void;
   }
 
   export interface StorageProviderInterface {
-    setItem(consumerKey: string, key: string, value: any): void;
-    getItem<R = any>(consumerKey: string, key: string): R;
+    setItem(consumerKey: string, key: string, value: any): Promise<void>;
+    getItem<R = any>(consumerKey: string, key: string): Promise<R | undefined>;
   }
 
 }
