657ef5eb0abcee13ffad70b95b694f2d35288b5f
Joël Charles <joel.charles91@gmail.com>
2018-05-25 14:34:19 +0200

    Add comments

--- src/react/index.ts
+++ src/react/index.ts
@@ -4,6 +4,13 @@ import { Consumer } from '../common';
 export namespace react {
 
   export interface ReactConsumer extends Consumer {
+    /**
+     * Will render the given children in specified destination
+     * @param {React.ComponentClass} children
+     * @param {react.ValidPortalIds} id
+     * @example
+     * sdk.react.createPortal(MyReactComponent, 'portal-quickswitch');
+     */
     createPortal(children: ComponentClass, id: react.ValidPortalIds): void;
     setProviderInterface(providerInterface: react.ReactProviderInterface): void;
   }
