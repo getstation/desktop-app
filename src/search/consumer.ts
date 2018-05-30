7e5e3af6511e030b0d6685cfd7051eca6bf379d1
Hugo Mano <hugo@hugomano.com>
2018-05-30 09:55:47 +0200

    Add id for SearchConsumer

--- src/search/consumer.ts
+++ src/search/consumer.ts
@@ -5,12 +5,14 @@ import { search } from './index';
 export class SearchConsumer extends Consumer implements search.SearchConsumer {
 
   public readonly namespace = 'search';
+  public id: string;
 
   public query: BehaviorSubject<search.SearchQuery>;
   public results: BehaviorSubject<search.SearchResultWrapper>;
 
-  constructor() {
+  constructor(id: string) {
     super();
+    this.id = id;
     this.query = new BehaviorSubject({ value: '' });
     this.results = new BehaviorSubject({});
   }
