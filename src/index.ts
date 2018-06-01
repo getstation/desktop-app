2db4309bb87c7341fed268eec38881dc6f902f88
Joël Charles <joel.charles91@gmail.com>
2018-06-01 13:32:02 +0200

    add Ipc to index

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import 'rxjs/Rx';
 import { SearchConsumer } from './search/consumer';
 import { StorageConsumer } from './storage/consumer';
 import { TabsConsumer } from './tabs/consumer';
+import { IpcConsumer } from './ipc/consumer';
 
 export * from './common';
 export * from './search';
@@ -9,7 +10,7 @@ export * from './storage';
 export * from './tabs';
 export * from './ipc';
 
-export type Consumers = SearchConsumer | StorageConsumer | TabsConsumer;
+export type Consumers = SearchConsumer | StorageConsumer | TabsConsumer | IpcConsumer;
 
 export interface SDKOptions {
   id: string,
@@ -20,6 +21,7 @@ export interface SDK {
   readonly search: SearchConsumer,
   readonly storage: StorageConsumer,
   readonly tabs: TabsConsumer,
+  readonly ipc: IpcConsumer,
   close(): void,
 }
 
@@ -32,17 +34,21 @@ export default function sdk(options: SDKOptions, provider: Provider): SDK {
   const search = new SearchConsumer(options.id);
   const storage = new StorageConsumer(options.id);
   const tabs = new TabsConsumer(options.id);
+  const ipc = new IpcConsumer(options.id);
   provider.register(search);
   provider.register(storage);
   provider.register(tabs);
+  provider.register(ipc);
   const bxsdk = {
     search,
     storage,
     tabs,
+    ipc,
     close() {
       provider.unregister(search);
       provider.unregister(storage);
       provider.unregister(tabs);
+      provider.unregister(ipc);
     },
   };
   Object.freeze(bxsdk);
