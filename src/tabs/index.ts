46650f1f1f0f56789cac53753d86481201734f35
Hugo Mano <hugo@hugomano.com>
2018-08-29 12:16:55 +0200

    getTabs => getTab

--- src/tabs/index.ts
+++ src/tabs/index.ts
@@ -13,13 +13,13 @@ export namespace tabs {
      */
     getTabs(): Tab[];
     /**
-     * Receive tabs updates
+     * Receive tab updates
      * @example
-     * sdk.tabs.getTabsObservable().subscribe(tabs => {
+     * sdk.tabs.getTab('mysaas-XXXXXXXXX/XXXXXXXXX').subscribe(tab => {
      *  ...
      * });
      */
-    getTabsObservable(id: string): Observable<tabs.Tab[]>;
+    getTab(id: string): Observable<tabs.Tab>;
     /**
      * Receive nav updates
      * @example
@@ -61,7 +61,7 @@ export namespace tabs {
 
   export interface TabsProviderInterface {
     getTabs(id: string): Tab[];
-    getTabsObservable(id: string): Observable<tabs.Tab[]>;
+    getTab(tabId: string): Observable<tabs.Tab>;
     nav(): Observable<tabs.Nav>;
     navToTab(tabId: string): Promise<void>
     executeJavaScript(tabId: string, code: string): Promise<void>;
