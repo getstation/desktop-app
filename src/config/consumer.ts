d20d3224a13704edc7d100d7b4e2c87833a2569a
Joël Charles <joel.charles91@gmail.com>
2019-03-13 13:14:50 +0100

    Slite plugin to update icon

---
+++ src/config/consumer.ts
@@ -0,0 +1,20 @@
+import { Consumer } from '../common';
+import { config } from './index';
+
+const protectedProvidersWeakMap = new WeakMap<ConfigConsumer, config.ConfigProviderInterface>();
+
+export class ConfigConsumer extends Consumer implements config.ConfigConsumer {
+  public readonly namespace = 'config';
+
+  get configData() {
+    return protectedProvidersWeakMap.get(this)!.configData.asObservable();
+  }
+
+  setProviderInterface(providerInterface: config.ConfigProviderInterface) {
+    protectedProvidersWeakMap.set(this, providerInterface);
+  }
+
+  setIcon(url: string): void {
+    return protectedProvidersWeakMap.get(this)!.setIcon(url);
+  }
+}
---
+++ src/config/index.ts
@@ -0,0 +1,37 @@
+import { Observable, Subject } from 'rxjs';
+import { Consumer } from '../common';
+
+export namespace config {
+
+  export interface ConfigConsumer extends Consumer {
+    readonly id: string;
+
+    /**
+     * Get configData for current application
+     * @example
+     *   sdk.config.configData
+     *    .subscribe(configData => {
+     *      // ... configData.subdomain
+     *    });
+     */
+    readonly configData: Observable<config.ConfigData>;
+
+    /**
+     * Update Dock icon for current application with given URL
+     * @example
+     *  sdk.config.setIcon('https://domain.tld/myicon.png');
+     */
+    setIcon(url: string): void;
+
+    setProviderInterface(providerInterface: config.ConfigProviderInterface): void;
+  }
+
+  export interface ConfigProviderInterface {
+    configData: Subject<config.ConfigData>;
+    setIcon(url: string): void;
+  }
+
+  export type ConfigData = {
+    subdomain?: string,
+  };
+}
