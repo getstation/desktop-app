434ea1ed0e1d724f60a08c883ee624bff5ab4e51
Joël Charles <joel.charles91@gmail.com>
2021-07-13 16:05:50 +0200

    open source panel

---
+++ app/settings/components/SettingsOpenSourceInfo.tsx
@@ -0,0 +1,63 @@
+import { Button, Size } from '@getstation/theme';
+import { remote } from 'electron';
+import * as React from 'react';
+// @ts-ignore: no declaration file
+import injectSheet from 'react-jss';
+
+export interface Classes {
+  container: string,
+  item: string,
+  title: string,
+  settingName: string,
+  button: string,
+}
+
+export interface Props {
+  classes?: Classes,
+}
+
+const styles = {
+  container: {
+    maxWidth: '600px',
+    paddingTop: '10px',
+    paddingBottom: '10px',
+  },
+  item: {
+  },
+  settingName: {
+    marginBottom: 8,
+    textTransform: 'uppercase',
+    fontSize: 14,
+    fontWeight: 'bold',
+  },
+  button: {
+    marginTop: 10,
+  },
+};
+
+@injectSheet(styles)
+export default class SettingsOpenSourceInfo extends React.PureComponent<Props, {}> {
+  render() {
+    const { classes } = this.props;
+
+    return (
+      <div className={classes!.container}>
+        <div className={classes!.item}>
+          <p className={classes!.settingName}>open source info</p>
+          <label>
+            This software is maintained by the open source community. If you’re a developer and want to contribute, check our Github.
+          </label>
+          <p>
+            <Button
+              onClick={() => remote.shell.openExternal('https://github.com/getstation/desktop-app')}
+              className={classes!.button}
+              btnSize={Size.XXSMALL}
+            >
+              Open Github
+            </Button>
+          </p>
+        </div>
+      </div>
+    );
+  }
+}
