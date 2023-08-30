acbbe5a2a4a542949e44eea8def9b9989cc09ab4
Viktor <vikad111@gmail.com>
2023-08-30 17:12:29 +0200

    #4 close about window

--- app/about-window/about.js
+++ app/about-window/about.js
@@ -1,9 +1,11 @@
-import '../utils/stat-cache';
-import '../dotenv';
 import { ipcRenderer } from 'electron';
+import * as remote from '@electron/remote';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
+
+import '../utils/stat-cache';
+import '../dotenv';
 import '../theme/css/app.global.css';
 import '../../node_modules/font-awesome/css/font-awesome.min.css';
 import configureStore from '../store/configureStore.client';
@@ -17,6 +19,18 @@ configureStore().then(store => {
 
   render(store);
 }).catch(handleError());
+
+const currentWindow = remote.getCurrentWindow();
+currentWindow.on('blur', (event, command) => {
+  currentWindow.close();
+});
+document.addEventListener('keydown', event => {
+  switch (event.key) {
+    case 'Escape':
+      currentWindow.close();
+      break;
+  }
+});
 
 const render = (store) => {
   const AboutWindowContainer = require('./Container').default; // eslint-disable-line global-require
@@ -34,4 +48,6 @@ const render = (store) => {
   ipcRenderer.send('bx-ready-to-show');
 };
 
-if (module.hot) { module.hot.accept(); }
+if (module.hot) { 
+  module.hot.accept(); 
+}
