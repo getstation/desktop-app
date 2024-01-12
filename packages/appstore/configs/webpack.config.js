7b09c919485b1c279ab7c0e10c430f7a313f9da2
Victor <vikad111@gmail.com>
2024-01-12 01:10:09 +0100

    in progress

--- packages/appstore/configs/webpack.config.js
+++ packages/appstore/configs/webpack.config.js
@@ -10,7 +10,7 @@ const PATHS = {
   root: path.resolve(__dirname, '..'),
   nodeModules: path.resolve(__dirname, '../node_modules'),
   src: path.resolve(__dirname, '../src'),
-  dist: path.resolve(__dirname, '../../dist/renderer/appstore'),
+  dist: path.resolve(__dirname, '../../../dist/renderer/appstore'),
   static: path.resolve(__dirname, '../src/static'),
 };
 
