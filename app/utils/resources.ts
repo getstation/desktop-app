f581e52e5887056eb0f113762898739f38eac2c4
Victor <vikad111@gmail.com>
2024-02-09 12:17:27 +0100

    Fix application icon in Linux

--- app/utils/resources.ts
+++ app/utils/resources.ts
@@ -1,13 +1,13 @@
 import * as fs from 'fs';
 import * as path from 'path';
 
-export const getResourcePath = (rootPath: string) => path.resolve(__dirname, '../..', rootPath);
+const getResourcePath = (rootPath: string) => path.resolve(__dirname, '../..', rootPath);
 
 /**
  * Returns the full path of the PNG icon of the app
  */
 export const getResourceIconPath = () => {
-  const stableIconPath = getResourcePath('build/icon_512x512.png');
+  const stableIconPath = getResourcePath('resources/icon.png');
   if (fs.existsSync(stableIconPath)) return stableIconPath;
 
   return undefined;
